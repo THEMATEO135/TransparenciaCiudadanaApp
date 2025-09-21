859f0e77340db1cb98b97e080b84156cba3e308b
caserranoa17-maker <caserranoa17@gmail.com>
2025-09-21 16:58:25 -0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator; 
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 👇 Activamos Bootstrap 5 para la paginación
+        Paginator::useBootstrapFive();
     }
 }
