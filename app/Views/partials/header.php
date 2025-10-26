f1b76245a855d5da79910bf809c383d25cbd71e6
caserrano17 <caserranoa17@gmail.com>
2025-10-26 11:04:11 -0500

    Corrigiendo .gitignore y agregando carpeta public

--- app/Views/partials/header.php
+++
@@ -1,7 +0,0 @@
-<header>
-    <h2>Transparencia Ciudadana</h2>
-    <nav>
-        <a href="/">Inicio</a> |
-        <a href="/admin">Administración</a>
-    </nav>
-</header>
--- config/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-'/' => ['App\\Controllers\\HomeController', 'index'],
-'/admin' => ['App\\Controllers\\AdminController', 'dashboard'],
-// Agrega más rutas aquí
-];
