d955ac235ec3115a9deadc333669d81c9678572c
THEMATEO135 <mateorodri_1997@hotmail.com>
2025-10-26 18:33:54 -0500

    aaa

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
         tailwindcss(),
     ],
     build: {
-        manifest: true,
+        manifest: 'manifest.json',
         outDir: 'public/build',
         rollupOptions: {
             output: {
